100b39ee6fb13374ec1404d625fa0c9924a15e58
PeterXRiber <perib21@student.sdu.dk>
2023-05-31 12:13:22 +0200

    Added Pre and Post conditions for IBullet

--- AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/perib21/common/services/IBulletPluginService.java
+++ AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/perib21/common/services/IBulletPluginService.java
@@ -5,6 +5,17 @@ import dk.sdu.mmmi.perib21.common.data.GameData;
 
 public interface IBulletPluginService {
 
+
+     /**
+      * @param gunner
+      * @param gameData
+      *
+      * Pre-condition: An entity that implements the SPI is loaded into the game
+      * and requires the ability to use the weapon thereby asking for a bullet to be
+      * created.
+      * Post-condition:
+      * The entity created a bullet and the bullet is now visible in the game.
+      */
      Entity create(Entity gunner, GameData gameData);
 
 
